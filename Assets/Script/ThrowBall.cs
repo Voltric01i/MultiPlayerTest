bc9e084c4586eec251b405ace22d6ddc05e8e507
Yuki <yupam0922@gmail.com>
2019-06-09 14:14:47 +0900

    ボール出現の実装

---
+++ Assets/Script/ThrowBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour
+{
+    public GameObject ThrowObject;
+    Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ThrowThing()
+    {
+        GameObject ThrowThing = Instantiate(ThrowObject);
+        ThrowObject.transform.position = mainCamera.transform.position;
+        var thR = ThrowThing.GetComponent<Rigidbody>();
+        thR.AddForce(ThrowThing.transform.forward,ForceMode.Impulse);
+    } 
+
+
+
+    
+
+}
